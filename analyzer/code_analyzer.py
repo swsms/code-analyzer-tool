6179dd6e78751b47604ce1697c467de8f931eb73
artyom <avburylov@gmail.com>
2020-01-05 15:22:10 +0500

    Add tests

--- analyzer/code_analyzer.py
+++ analyzer/code_analyzer.py
@@ -1,6 +1,104 @@
-file_path = input()
-with open(file_path, mode='r') as file:
-    for (num, line) in enumerate(file, 1):
-        prepared_line = line.strip()
-        if len(prepared_line) > 79:
-            print(f'Line {num}: S001 Too long line')
+from typing import List, Tuple
+
+TOO_LONG_CODE = 'S001'
+TOO_LONG_LINE_MSG = 'Too long line'
+
+INDENTATION_CODE = 'S002'
+INDENTATION_MSG = 'Indentation is not a multiple of four'
+
+END_NEW_LINE_MISSING_CODE = 'S003'
+END_NEW_LINE_MISSING_MSG = 'No newline at the end of the file'
+
+UNNECESSARY_SEMICOLON_CODE = 'S0011'
+UNNECESSARY_SEMICOLON_MSG = 'The line ends with an unnecessary semicolon'
+
+TODO_CODE = 'S004'
+TODO_CODE_MSG = 'TODO found'
+
+#
+
+#
+# NON ASCII CHARACTER (COLUMN?)
+#
+# check 4 TOO MANY BLANK LINES (3)
+
+
+def read_code_lines(file_path: str) -> List[str]:
+    with open(file_path, mode='r') as file:
+        return [line.strip() for line in file.readlines()]
+
+
+def analyze_code_lines(lines: List[str]) -> List[Tuple[int, str, str]]:
+    violations = []
+
+    for (num, line) in enumerate(lines, 1):
+        if check_line_is_too_long(line):
+            violations.append((num, TOO_LONG_CODE, TOO_LONG_LINE_MSG))
+
+        if check_indentation_is_not_multiple_of_four(line):
+            violations.append((num, INDENTATION_CODE, INDENTATION_MSG))
+
+        if has_unnecessary_semicolon(line):
+            violations.append((num, UNNECESSARY_SEMICOLON_CODE,
+                               UNNECESSARY_SEMICOLON_MSG))
+
+        if find_todo_within_comment(line):
+            violations.append((num, TODO_CODE, TODO_CODE_MSG))
+
+    return violations
+
+
+def check_line_is_too_long(line: str) -> bool:
+    return len(line) > 79
+
+
+def check_indentation_is_not_multiple_of_four(line: str) -> bool:
+    spaces_count = 0
+    char_number = 0
+    while char_number < len(line):
+        if line[char_number] == ' ':
+            spaces_count += 1
+        else:
+            break
+        char_number += 1
+    return char_number % 4 != 0
+
+
+def find_positions_with_too_many_blank_lines(lines: List[str]) -> List[int]:
+    indexes = []
+    for index in range(2, len(lines)):
+        if not (lines[index - 1].strip() or lines[index - 2].strip()):
+            indexes.append(index)
+    return indexes
+
+
+def find_all_non_ascii_chars(line: str) -> List[Tuple[int, str]]:
+    return [(index, str(char))
+            for index, char in enumerate(line, 1)
+            if not char.isascii()]
+
+
+def has_unnecessary_semicolon(line: str) -> bool:
+    return line.endswith(';')
+
+
+def find_todo_within_comment(line: str) -> bool:
+    todo_index = line.lower().find('todo')
+    comment_sign_index = line.find('#')
+    return todo_index >= 0 and 0 <= comment_sign_index < todo_index
+
+
+def print_violations(violations: List[Tuple[int, str, str]]) -> None:
+    for violation in violations:
+        print(f'Line {violation[0]}: {violation[1]} {violation[2]}')
+
+
+if __name__ == '__main__':
+    file_path = input()
+    code_lines = read_code_lines(file_path)
+    violations = analyze_code_lines(code_lines)
+    print_violations(violations)
+
+
+
+
--- tests/test_code_analyzer.py
+++ tests/test_code_analyzer.py
@@ -1,33 +1,31 @@
-import subprocess
-
-PATH_TO_SCRIPT = 'analyzer/code_analyzer.py'
-PATH_TO_SAMPLES_DIR = 'tests/samples'
-ENCODING = 'utf-8'
+from analyzer.code_analyzer import (
+    check_line_is_too_long, check_indentation_is_not_multiple_of_four
+)
 
 
-def run_script_with_stdin(stdin_as_string) -> str:
-    process = subprocess.Popen(['python', PATH_TO_SCRIPT],
-                               stdin=subprocess.PIPE,
-                               stdout=subprocess.PIPE)
-    stdout = process.communicate(input=stdin_as_string.encode(ENCODING))[0]
-    return stdout.decode(ENCODING)
+def test_check_line_is_too_long():
+    line = "print('hello')"
+    assert not check_line_is_too_long(line)
+
+    line = "print('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa')"
+    assert check_line_is_too_long(line)
 
 
-def test_code_analyzer_when_single_line_without_issues():
-    result = run_script_with_stdin(f'{PATH_TO_SAMPLES_DIR}/single_line_valid_example.py')
-    assert not result
+def test_check_indentation_is_not_multiple_of_four():
+    line = "print('hello')"
+    assert not check_indentation_is_not_multiple_of_four(line)
 
+    line = " print('hello')"
+    assert check_indentation_is_not_multiple_of_four(line)
 
-def test_code_analyzer_when_too_long_single_line():
-    result = run_script_with_stdin(f'{PATH_TO_SAMPLES_DIR}/single_long_line_example.py')
-    issues = result.splitlines()
-    assert len(issues) == 1
-    assert issues[0].startswith('Line 1: S001')
+    line = "  print('hello')"
+    assert check_indentation_is_not_multiple_of_four(line)
 
+    line = "    print('hello')"
+    assert not check_indentation_is_not_multiple_of_four(line)
 
-def test_when_multiple_long_lines():
-    result = run_script_with_stdin(f'{PATH_TO_SAMPLES_DIR}/multiple_long_lines.py')
-    issues = result.splitlines()
-    assert len(issues) == 2
-    assert issues[0].startswith('Line 3: S001')
-    assert issues[1].startswith('Line 5: S001')
+    line = "       print('hello')"
+    assert check_indentation_is_not_multiple_of_four(line)
+
+    line = "        print('hello')"
+    assert not check_indentation_is_not_multiple_of_four(line)
